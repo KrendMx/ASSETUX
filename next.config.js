5e9e73341de9a55e02f2c62a3d9d2baa85011474
Kirill <igorlfmartins@mail.ru>
2021-12-25 11:24:32 +0300

    Add yarn & add preact for bundle size optimization

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const { i18n } = require("./next-i18next.config")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
 
-module.exports = {
+const webpack = (config, { dev, isServer }) => {
+  if (!dev && !isServer) {
+    Object.assign(config.resolve.alias, {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime"
+    })
+  }
+
+  return config
+}
+
+module.exports = withBundleAnalyzer({
   async headers() {
     return [
       {
@@ -17,9 +33,10 @@ module.exports = {
       }
     ]
   },
-  reactStrictMode: true,
+  webpack,
   i18n,
+  reactStrictMode: true,
   images: {
     domains: ["bscscan.com", "assetux.com"]
   }
-}
+})
