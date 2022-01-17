2d67fcd990f505f35aecc2c12b5f8ed9596cdc8f
Kirill <igorlfmartins@mail.ru>
2022-01-17 21:36:15 +0300

    Intend styles & don't show a blue background on touch

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -9,61 +9,62 @@ export const mobileHeaderHeight = 63
 
 const GlobalStyles = createGlobalStyle`
   :root {
-  --blue: #0066cc;
-  --green: #68cc45;
-  --red: #ff7979;
-  --gray: #989898;
-  --lightgray: #f5f5f5;
-  --black: #3a3a3a;
-  --white: #ffffff;
-  --bgColor: #ffffff;
-  --opacity: 0.3;
-  --max-width: ${maxWidth}px;
-  --paddings: ${desktopPaddings}px;
-  --header-height: ${headerHeight}px;
-}
-
-*,
-*::after,
-*::before {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-html {
-  font-size: 19px;
-}
-
-html,
-button,
-input {
-  font-family: "SF Pro Display", sans-serif;
-}
-
-.skeletonFlexContainer {
-  flex-grow: 1;
-}
-
-.skeletonResetLineHeight {
-  line-height: normal;
-}
-
-@media only screen and (max-width: ${tablet}px) {
-  :root {
-    --paddings: ${tabletPaddings}px;
+    --blue: #0066cc;
+    --green: #68cc45;
+    --red: #ff7979;
+    --gray: #989898;
+    --lightgray: #f5f5f5;
+    --black: #3a3a3a;
+    --white: #ffffff;
+    --bgColor: #ffffff;
+    --opacity: 0.3;
+    --max-width: ${maxWidth}px;
+    --paddings: ${desktopPaddings}px;
+    --header-height: ${headerHeight}px;
   }
-}
 
-@media only screen and (max-width: ${mobile}px) {
-  :root {
-    --header-height: ${mobileHeaderHeight}px;
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-tap-highlight-color: transparent;
   }
 
   html {
-    font-size: 15px;
+    font-size: 19px;
   }
-}
+
+  html,
+  button,
+  input {
+    font-family: "SF Pro Display", sans-serif;
+  }
+
+  .skeletonFlexContainer {
+    flex-grow: 1;
+  }
+
+  .skeletonResetLineHeight {
+    line-height: normal;
+  }
+
+  @media only screen and (max-width: ${tablet}px) {
+    :root {
+      --paddings: ${tabletPaddings}px;
+    }
+  }
+
+  @media only screen and (max-width: ${mobile}px) {
+    :root {
+      --header-height: ${mobileHeaderHeight}px;
+    }
+
+    html {
+      font-size: 15px;
+    }
+  }
 `
 
 export default GlobalStyles
