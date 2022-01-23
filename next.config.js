5086d5af65b851507afb6261109a63378ed79b17
Kirill <igorlfmartins@mail.ru>
2022-01-23 22:29:53 +0300

    Use plugin for preact

--- next.config.js
+++ next.config.js
@@ -1,49 +1,38 @@
 /** @type {import('next').NextConfig} */
 
+const withPreact = require("next-plugin-preact")
 const withPWA = require("next-pwa")
 const { i18n } = require("./next-i18next.config")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
-const webpack = (config, { dev, isServer }) => {
-  if (!dev && !isServer) {
-    Object.assign(config.resolve.alias, {
-      react: "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-      "react/jsx-runtime": "preact/jsx-runtime"
-    })
-  }
-
-  return config
-}
-
 module.exports = withBundleAnalyzer(
-  withPWA({
-    async headers() {
-      return [
-        {
-          source: "/:all*(woff2|woff|ttf|eof)",
-          locale: false,
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=9999999999, must-revalidate"
-            }
-          ]
-        }
-      ]
-    },
-    webpack,
-    i18n,
-    reactStrictMode: true,
-    images: {
-      domains: ["bscscan.com", "assetux.com"]
-    },
-    pwa: {
-      dest: "public",
-      disable: true
-    }
-  })
+  withPWA(
+    withPreact({
+      async headers() {
+        return [
+          {
+            source: "/:all*(woff2|woff|ttf|eof)",
+            locale: false,
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=9999999999, must-revalidate"
+              }
+            ]
+          }
+        ]
+      },
+      i18n,
+      reactStrictMode: true,
+      images: {
+        domains: ["bscscan.com", "assetux.com"]
+      },
+      pwa: {
+        dest: "public",
+        disable: true
+      }
+    })
+  )
 )
