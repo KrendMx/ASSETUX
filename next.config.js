e9d23bea7a329930f9f9b32b5150fe3cabff593a
Kirill <igorlfmartins@mail.ru>
2022-01-23 14:17:13 +0300

    Add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+const withPWA = require("next-pwa")
 const { i18n } = require("./next-i18next.config")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
@@ -18,25 +19,31 @@ const webpack = (config, { dev, isServer }) => {
   return config
 }
 
-module.exports = withBundleAnalyzer({
-  async headers() {
-    return [
-      {
-        source: "/:all*(woff2|woff|ttf|eof)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=9999999999, must-revalidate"
-          }
-        ]
-      }
-    ]
-  },
-  webpack,
-  i18n,
-  reactStrictMode: true,
-  images: {
-    domains: ["bscscan.com", "assetux.com"]
-  }
-})
+module.exports = withBundleAnalyzer(
+  withPWA({
+    async headers() {
+      return [
+        {
+          source: "/:all*(woff2|woff|ttf|eof)",
+          locale: false,
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=9999999999, must-revalidate"
+            }
+          ]
+        }
+      ]
+    },
+    webpack,
+    i18n,
+    reactStrictMode: true,
+    images: {
+      domains: ["bscscan.com", "assetux.com"]
+    },
+    pwa: {
+      dest: "public",
+      disable: true
+    }
+  })
+)
