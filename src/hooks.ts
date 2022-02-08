67b1bf3edb2e74c896508639ed2982e85109101f
Kirill <igorlfmartins@mail.ru>
2022-02-08 22:59:22 +0300

    usePrevious

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,14 @@
-import { useEffect, useLayoutEffect } from "react"
+import { useEffect, useLayoutEffect, useRef } from "react"
 
 export const useIsomorphicLayoutEffect =
   typeof window === "undefined" ? useEffect : useLayoutEffect
+
+export const usePrevious = <T>(value: T) => {
+  const ref = useRef<T>()
+
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+
+  return ref.current
+}
