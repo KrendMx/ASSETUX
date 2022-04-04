e1ed2ef53e7649ae40593d471284456f30f82ddb
Kirill <igorlfmartins@mail.ru>
2022-04-04 10:05:52 +0300

    Update mobile menu

--- components/Menus/Burger/NavGroup.tsx
+++ components/Menus/Burger/NavGroup.tsx
@@ -10,9 +10,10 @@ import type { Route } from "@/src/routes"
 type NavGroupProps = {
   title: string
   routes: Route[]
+  onClick?: (route: Route) => void
 }
 
-function NavGroup({ title, routes }: NavGroupProps) {
+function NavGroup({ title, routes, onClick }: NavGroupProps) {
   const [active, setActive] = useState(false)
   const { t } = useTranslation("routes")
 
@@ -27,9 +28,35 @@ function NavGroup({ title, routes }: NavGroupProps) {
         <ul>
           {routes.map((route, index) => (
             <li key={index}>
-              <Link href={route.href} passHref>
-                <NavLink>{t(route.key)}</NavLink>
-              </Link>
+              {route.absolute && (
+                <NavLink
+                  href={route.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={(event) => {
+                    if (onClick) {
+                      event.preventDefault()
+                      onClick(route)
+                    }
+                  }}
+                >
+                  {t(route.key)}
+                </NavLink>
+              )}
+              {!route.absolute && (
+                <Link href={route.href} passHref>
+                  <NavLink
+                    onClick={(event) => {
+                      if (onClick) {
+                        event.preventDefault()
+                        onClick(route)
+                      }
+                    }}
+                  >
+                    {t(route.key)}
+                  </NavLink>
+                </Link>
+              )}
             </li>
           ))}
         </ul>
