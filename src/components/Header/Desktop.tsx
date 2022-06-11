cc1c23d7cd1a0873e85139117394c342f5d06261
adjsky <igorlfmartins@mail.ru>
2022-06-11 19:23:26 +0300

    Simplify checks

--- src/components/Header/Desktop.tsx
+++ src/components/Header/Desktop.tsx
@@ -80,11 +80,9 @@ function Desktop() {
 
   const isMainPage = router.pathname == "/"
   const isCommercePage =
-    (router.pathname.includes("profile") ||
-      router.pathname.includes("bill") ||
-      router.pathname.includes("history")) &&
-    !router.pathname.includes("login")
-  const isCommerceLogin = router.pathname == `${getEcommercePrefix()}/login`
+    router.pathname.startsWith("/profile") &&
+    router.pathname != "/profile/login"
+  const isCommerceLogin = router.pathname == "/profile/login"
 
   return (
     <DesktopContainer>
