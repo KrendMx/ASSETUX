3d0fe738166b3eacb84012095a479538e06a4153
Mikhail Likhachev <lihahasheshki@gmail.com>
2022-08-24 14:57:50 +0300

    remove

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,25 +1,10 @@
-const locales = require("./locales")
-const config = {
-  defaultLocale: "ru",
-  locales,
-  localeDetection: true
-}
+const locales = require("./locales.js")
 
 module.exports = {
   i18n: {
-    ...config,
-    localeExtension: "yaml",
-    domains: [
-      {
-        domain: "assetux.com",
-        defaultLocale: "ru"
-      },
-      {
-        domain: "commerce.assetux.com",
-        defaultLocale: "ru"
-      }
-    ]
-  },
-  config,
-  nonExplicitSupportedLngs: true
+    defaultLocale: "ru",
+    locales,
+    localeDetection: true,
+    localeExtension: "yaml"
+  }
 }
