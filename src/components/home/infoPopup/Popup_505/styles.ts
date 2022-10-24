8e35dac6e1b9e601ec03dac7fe59a5c618df5965
Maxim Krendel <64363842+KrendMx@users.noreply.github.com>
2022-10-24 12:21:36 +0300

    fix

---
+++ src/components/home/infoPopup/Popup_505/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const BackgroundLocal = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.35);
+`
+
+const ScrolledBlock = styled.div<{ height: string }>`
+  max-height: ${(props) => props.height}!important;
+  width: 100%;
+  overflow-y: scroll;
+`
+
+export { BackgroundLocal, ScrolledBlock }
