ba843354e50587d4031cc12c5925738a2384f1db
Maxim Krendel <64363842+KrendMx@users.noreply.github.com>
2022-11-09 18:43:24 +0200

    first commit

--- src/lib/backend/clients.ts
+++
@@ -1,5 +0,0 @@
-import { EcommerceClient as _EcommerceClient } from './ecommerce'
-import { BackendClient as _BackendClient } from './main'
-
-export const BackendClient = new _BackendClient()
-export const EcommerceClient = new _EcommerceClient()
--- src/lib/utils/date.ts
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs'
-import 'dayjs/locale/ru'
-
-export const getFormattedDate = (date: string | number, locale: string) =>
-  dayjs(date).locale(locale).format('HH:mm, D MMMM YYYY')
